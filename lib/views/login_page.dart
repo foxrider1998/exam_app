c1778bebbd61bc0c64a6161c1900062031058a91
Aris <areskretindo@gmail.com>
2022-10-06 18:28:33 +0700

    ios icon

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -30,39 +30,10 @@ class _LoginPageState extends State<LoginPage> {
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
-
+    print("credensial");
+    print(credential);
     // Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithCredential(credential);
-
-    // Future<UserCredential> signInWithGoogle() async {
-
-    //   print("sini ok");
-    //   // if (kIsWeb) {
-    //   //   GoogleAuthProvider googleProvider = GoogleAuthProvider();
-
-    //   //   googleProvider
-    //   //       .addScope('https://www.googleapis.com/auth/contacts.readonly');
-    //   //   googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
-
-    //   //   // Once signed in, return the UserCredential
-    //   //   return await FirebaseAuth.instance.signInWithPopup(googleProvider);
-    //   // } else {
-    //   //   // Trigger the authentication flow
-    //   //   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    //   //   // Obtain the auth details from the request
-    //   //   final GoogleSignInAuthentication? googleAuth =
-    //   //       await googleUser?.authentication;
-
-    //   //   // Create a new credential
-    //   //   final credential = GoogleAuthProvider.credential(
-    //   //     accessToken: googleAuth?.accessToken,
-    //   //     idToken: googleAuth?.idToken,
-    //   //   );
-
-    //     // Once signed in, return the UserCredential
-    //     return await FirebaseAuth.instance.signInWithCredential(credential);
-    //   }
   }
 
   @override
@@ -121,6 +92,7 @@ class _LoginPageState extends State<LoginPage> {
                 await signInWithGoogle();
 
                 final user = FirebaseAuth.instance.currentUser;
+
                 if (user != null) {
                   print("sini print usernya");
                   print(user);
